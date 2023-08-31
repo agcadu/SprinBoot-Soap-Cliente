cfe04586c68a6541787c96902ffcc4a64f79b2d3
Agustin <agcadu@gmail.com>
2023-08-31 20:49:57 +0200

    modifico controller

--- src/main/java/com/soap/controller/SoapController.java
+++ src/main/java/com/soap/controller/SoapController.java
@@ -8,6 +8,7 @@ import com.soap.wsdl.SubtractResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class SoapController {
 
     @Autowired
